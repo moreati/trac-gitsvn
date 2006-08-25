moreati/trac-gitsvn

commit c4383fe7f52e581fd7efd65ae9315ec01aec83a8

Improve PHP highlighter by applying Tim Hatch's patch from #3326.
git-svn-id: http://svn.edgewall.org/repos/trac/trunk@3488 af82e41b-90c4-0310-8c96-b1721e28e2e2

trac/mimeview/php.py
@@ -16,6 +16,8 @@
 # Author: Christian Boos <cboos@bct-technology.com>
 #         Christopher Lenz <cmlenz@gmx.de>
 
+import re
+
 from trac.core import *
 from trac.config import Option
 from trac.mimeview.api import IHTMLPreviewRenderer, content_to_unicode
@@ -30,14 +32,27 @@ php_types = ('text/x-php', 'application/x-httpd-php',
 
 class PhpDeuglifier(Deuglifier):
 
+    def format(self, indata):
+        # The PHP highlighter produces the end-span tags on the next line
+        # instead of the line they actually apply to, which causes
+        # Trac to produce lots of (useless) open-and-immediately-close
+        # spans beginning each line.  This tries to curtail by bubbling
+        # the first span after a set of 1+ "<br />" to before them.
+        r_fixeol = re.compile(r"((?:<br />)+)(</(?:font|span)>)")
+        indata = r_fixeol.sub(lambda m: m.group(2) + m.group(1), indata)
+        
+        # Now call superclass implementation that handles the dirty work
+        # of applying css classes.
+        return Deuglifier.format(self, indata)
+
     def rules(cls):
         colors = dict(comment='FF8000', lang='0000BB', keyword='007700',
                       string='DD0000')
         # rules check for <font> for PHP 4 or <span> for PHP 5
         color_rules = [
-                r'(?P<%s><(?:font color="|span style="color: )#%s">)' % c
-                for c in colors.items()
-                ]
+            r'(?P<%s><(?:font color="|span style="color: )#%s">)' % c
+            for c in colors.items()
+            ]
         return color_rules + [ r'(?P<font><font.*?>)', r'(?P<endfont></font>)' ]
     rules = classmethod(rules)
 
@@ -71,7 +86,7 @@ class PHPRenderer(Component):
             err = 'Running (%s) failed: %s, %s.' % (cmdline, np.errorlevel,
                                                     np.err)
             raise Exception, err
-        odata = ''.join(np.out.splitlines()[1:-1])
+        odata = ''.join(np.out.splitlines()[1:-2])
         if odata.startswith('X-Powered-By'):
             raise TracError, 'You appear to be using the PHP CGI binary.  ' \
                              'Trac requires the CLI version for syntax ' \

commit 17d4ad8505978a5cd1a28a7db8ea4fee4b53bc3f

PHP renderer improvements and test suite added. This also fixes a regression introduced in r3488. Closes #3624. Many thanks to Tim Hatch for the contributed patch.
git-svn-id: http://svn.edgewall.org/repos/trac/trunk@3655 af82e41b-90c4-0310-8c96-b1721e28e2e2

trac/mimeview/php.py
@@ -16,6 +16,7 @@
 # Author: Christian Boos <cboos@bct-technology.com>
 #         Christopher Lenz <cmlenz@gmx.de>
 
+import os
 import re
 
 from trac.core import *
@@ -82,15 +83,21 @@ class PHPRenderer(Component):
         content = content_to_unicode(self.env, content, mimetype)
         content = content.encode('utf-8')
         np = NaivePopen(cmdline, content, capturestderr=1)
-        if np.errorlevel or np.err:
+        if (os.name != 'nt' and np.errorlevel) or np.err:
             err = 'Running (%s) failed: %s, %s.' % (cmdline, np.errorlevel,
                                                     np.err)
             raise Exception, err
-        odata = ''.join(np.out.splitlines()[1:-2])
+        odata = ''.join(np.out.splitlines()[1:-1])
         if odata.startswith('X-Powered-By'):
             raise TracError, 'You appear to be using the PHP CGI binary.  ' \
                              'Trac requires the CLI version for syntax ' \
                              'highlighting.'
+
+        epilogues = ["</span>", "</font>"]
+        for e in epilogues:
+            if odata.endswith(e):
+                odata = odata[:-len(e)]
+                break
 
         html = PhpDeuglifier().format(odata.decode('utf-8'))
         for line in html.split('<br />'):

trac/test.py
@@ -164,6 +164,21 @@ class EnvironmentStub(Environment):
         return self.known_users
 
 
+def locate(fn):
+    """Locates a binary on the path.
+
+    Returns the fully-qualified path, or None.
+    """
+    import os
+    exec_suffix = os.name == 'nt' and '.exe' or ''
+    
+    for p in ["."] + os.environ['PATH'].split(os.pathsep):
+        f = os.path.join(p, fn + exec_suffix)
+        if os.path.exists(f):
+            return f
+    return None
+
+
 def suite():
     import trac.tests
     import trac.db.tests

trac/mimeview/tests/__init__.py
@@ -1,10 +1,11 @@
-from trac.mimeview.tests import api
+from trac.mimeview.tests import api, php
 
 import unittest
 
 def suite():
     suite = unittest.TestSuite()
     suite.addTest(api.suite())
+    suite.addTest(php.suite())
     return suite
 
 if __name__ == '__main__':

trac/mimeview/tests/php.py
@@ -1,0 +1,148 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2006 Edgewall Software
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution. The terms
+# are also available at http://trac.edgewall.org/license.html.
+#
+# This software consists of voluntary contributions made by many
+# individuals. For the exact contribution history, see the revision
+# history and logs, available at http://trac.edgewall.org/trac/.
+#
+
+import unittest
+from trac.test import EnvironmentStub, locate
+from StringIO import StringIO
+
+from trac.mimeview.api import Mimeview
+from trac.mimeview.php import PhpDeuglifier, PHPRenderer
+
+class PhpDeuglifierTestCase(unittest.TestCase):
+
+    def test_nomarkup(self):
+        self.assertEqual('asd', PhpDeuglifier().format('asd'))
+
+    def test_rewrite_span(self):
+        self.assertEqual('<span class="code-comment">asd</span>',
+            PhpDeuglifier().format('<span style="color: #FF8000">asd</span>'))
+        self.assertEqual('<span class="code-lang">asd</span>',
+            PhpDeuglifier().format('<span style="color: #0000BB">asd</span>'))
+        self.assertEqual('<span class="code-keyword">asd</span>',
+            PhpDeuglifier().format('<span style="color: #007700">asd</span>'))
+        self.assertEqual('<span class="code-string">asd</span>',
+            PhpDeuglifier().format('<span style="color: #DD0000">asd</span>'))
+
+    def test_rewrite_font(self):
+        self.assertEqual('<span class="code-comment">asd</span>',
+            PhpDeuglifier().format('<font color="#FF8000">asd</font>'))
+        self.assertEqual('<span class="code-lang">asd</span>',
+            PhpDeuglifier().format('<font color="#0000BB">asd</font>'))
+        self.assertEqual('<span class="code-keyword">asd</span>',
+            PhpDeuglifier().format('<font color="#007700">asd</font>'))
+        self.assertEqual('<span class="code-string">asd</span>',
+            PhpDeuglifier().format('<font color="#DD0000">asd</font>'))
+
+    def test_reorder_br(self):
+        """
+        Regression test for #3326 point 2 (close tags after line break)
+        """
+        self.assertEqual('<span class="code-lang"></span><br />',
+            PhpDeuglifier().format(
+                '<span style="color: #0000BB"><br /></span>'))
+        self.assertEqual('<span class="code-lang"></span><br /><br />',
+            PhpDeuglifier().format(
+                '<span style="color: #0000BB"><br /><br /></span>'))
+
+class PhpRendererTestCase(unittest.TestCase):
+
+    def _test_render(self, stuff, type="string"):
+        env = EnvironmentStub(enable=[PHPRenderer])
+        m = Mimeview(env)
+        r = m.renderers[0]
+        if type=="string":
+            s = stuff
+        elif type=="file":
+            s = StringIO(stuff)
+        else:
+            raise NotImplementedException(
+                "Pass either type=file or type=string")
+        result = list(r.render(None, None, s))
+        return result
+
+    def test_boring_string(self):
+        """
+        Simple regression test for #3624 (php chops off the last line)
+        """
+        result = self._test_render('asd')
+        self.assertEqual('asd', result[0])
+        self.assertEqual(1, len(result))
+
+    def test_boring_filelike(self):
+        """
+        Regression test for #3261 (treats content as string) # FIXME see #3332
+        """
+        result = self._test_render('asd', 'file')
+        self.assertEqual('asd', result[0])
+        self.assertEqual(1, len(result))
+
+    def test_simple_string(self):
+        result = self._test_render('<?php\n?>')
+        self.assertEqual('<span class="code-lang">&lt;?php', result[0])
+        self.assertEqual('?&gt;</span>', result[1])
+        self.assertEqual(2, len(result))
+
+    def test_simple_unicode(self):
+        result = self._test_render(u'<?php echo "\u00e9"; ?>')
+        self.assertEqual(u'<span class="code-lang">&lt;?php </span>'
+                         u'<span class="code-keyword">echo </span>'
+                         u'<span class="code-string">"é"</span>'
+                         u'<span class="code-keyword">; </span>'
+                         u'<span class="code-lang">?&gt;</span>', result[0])
+        self.assertEqual(1, len(result))
+    
+    def test_way_too_many_nbsp(self):
+        """
+        Regression test for a tiny part of #1676
+        """
+        result = self._test_render('<?php\n    ?>')
+        self.assertEqual('<span class="code-lang">&lt;?php', result[0])
+        self.assertEqual('    ?&gt;</span>', result[1])
+        self.assertEqual(2, len(result))
+
+    def test_deuglified_reorder_br(self):
+        """
+        If the reordering of <br /> and the subsequent orphan </span>
+        isn't working, the </span> appears at the beginning of the third
+        line instead of the end of the second.
+        """
+        result = self._test_render('<?php\n$x="asd";\n?>')
+        self.assertEqual('<span class="code-lang">&lt;?php', result[0])
+        self.assertEqual('$x</span><span class="code-keyword">=</span>'
+            '<span class="code-string">"asd"</span>'
+            '<span class="code-keyword">;</span>', result[1])
+        self.assertEqual('<span class="code-lang">?&gt;</span>', result[2])
+        self.assertEqual(3, len(result))
+
+    def test_keeps_last_line(self):
+        """
+        More complex regression test for #3624 (php chops off the last line)
+        """
+        result = self._test_render('<p />\n<p />')
+        self.assertEqual('&lt;p /&gt;', result[0])
+        self.assertEqual('&lt;p /&gt;', result[1])
+        self.assertEqual(2, len(result))
+
+def suite():
+    suite = unittest.TestSuite()
+    php = locate("php")
+    if php:
+        suite.addTest(unittest.makeSuite(PhpDeuglifierTestCase, 'test'))
+        suite.addTest(unittest.makeSuite(PhpRendererTestCase, 'test'))
+    else:
+        print "SKIP: mimeview/tests/php.py"
+    return suite
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='suite')